8d072a5b546b74ecd7284e66e9d534683953c8dc
Berkay <berkaycirak_11@hotmail.com>
2022-06-21 17:06:18 +0300

    First Commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+function Login() {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src='/assets/images/cta-logo-one.svg' alt='' />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 18vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('/assets/images/login-background.jpg');
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  display: block;
+  margin-bottom: 12px;
+  min-height: 100px;
+  max-width: 600px;
+  width: 100%;
+`;
+
+export default Login;
