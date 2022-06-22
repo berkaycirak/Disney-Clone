0319f248bffa351c51e31f0bf3f5a83b001058c0
Berkay <berkaycirak_11@hotmail.com>
2022-06-22 13:33:03 +0300

    Firebase is added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
+import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <Header />
         <Routes>
           <Route exact path='/' element={<Login />} />
         </Routes>
       </Router>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlUzjQC6ZFBArUxlitsy59XdiTXQbmxeY',
+  authDomain: 'disney-clone-b6427.firebaseapp.com',
+  projectId: 'disney-clone-b6427',
+  storageBucket: 'disney-clone-b6427.appspot.com',
+  messagingSenderId: '880303920953',
+  appId: '1:880303920953:web:590d888ed186f3e0fd3ae8',
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
