a9b8d80c1a1c05415985013dee9d1cb3ad437860
Berkay <berkaycirak_11@hotmail.com>
2022-06-22 16:53:00 +0300

    Viewers page is added

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -3,11 +3,42 @@ import styled from 'styled-components';
 function Viewers(props) {
   return (
     <Container>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
+      <Wrap>
+        <img src='/assets/images/viewers-disney.png' alt='' />
+        <video autoPlay loop playsInline>
+          <source src='/assets/videos/1564674844-disney.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src='/assets/images/viewers-pixar.png' alt='' />
+        <video autoPlay loop playsInline>
+          <source src='/assets/videos/1564676714-pixar.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src='/assets/images/viewers-marvel.png' alt='' />
+        <video autoPlay loop playsInline>
+          <source src='/assets/videos/1564676115-marvel.mp4' type='video/mp4' />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src='/assets/images/viewers-starwars.png' alt='' />
+        <video autoPlay loop playsInline>
+          <source
+            src='/assets/videos/1608229455-star-wars.mp4'
+            type='video/mp4'
+          />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src='/assets/images/viewers-national.png' alt='' />
+        <video autoPlay loop playsInline>
+          <source
+            src='/assets/videos/1564676296-national-geographic.mp4'
+            type='video/mp4'
+          />
+        </video>
+      </Wrap>
     </Container>
   );
 }
@@ -22,6 +53,37 @@ const Container = styled.div`
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`;
+
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 25px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 18px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.45, 0.45, 0.94);
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out;
+    width: 100%;
+    z-index: 1;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    inset: 0px;
   }
 `;
 
