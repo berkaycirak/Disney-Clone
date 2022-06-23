204ce34ea1abd01a4da98a189dab81de8b0fb0e3
Berkay <berkaycirak_11@hotmail.com>
2022-06-23 15:53:01 +0300

    Homepage is fixed

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
 function Detail() {
-  return <Container>Detail</Container>;
+  return (
+    <Container>
+      <Background>
+        <img
+          src='https://images.unsplash.com/photo-1599056377758-4808a7e70337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=866&q=80'
+          alt=''
+        />
+      </Background>
+      <ImageTitle>
+        <img src='' alt='' />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src='/assets/images/play-icon-black.png' alt='' />
+            <span>Play</span>
+          </Player>
+        </Controls>
+      </ContentMeta>
+    </Container>
+  );
 }
 
 const Container = styled.div`
@@ -11,6 +31,91 @@ const Container = styled.div`
   display: block;
   top: 72px;
   padding: 0 calc(3.5vw + 5px);
+`;
+
+const Background = styled.div`
+  left: 0px;
+  opacity: 0.8;
+  position: fixed;
+  right: 0px;
+  top: 0px;
+  z-index: -1;
+
+  img {
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-width: 768px) {
+      width: initial;
+    }
+  }
+`;
+
+const ImageTitle = styled.div`
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  margin: 0px auto;
+  height: 30vh;
+  min-height: 170px;
+  padding-bottom: 24px;
+  width: 100%;
+
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
+`;
+
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0px 22px 0px 0px;
+  padding: 0px 24px;
+  height: 56px;
+  border-radius: 4px;
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb(249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  img {
+    width: 32px;
+  }
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    font-size: 12px;
+    margin: 0px 10px 0px 0px;
+
+    img {
+      width: 25px;
+    }
+  }
 `;
 
 export default Detail;
